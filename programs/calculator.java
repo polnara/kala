d1df8e51f933d116ce1e8025c4fc78a80e43c313
Narasaiah <narsimcagips@gmail.com>
2020-09-19 20:17:21 +0530

    added functions logic

--- programs/calculator.java
+++ programs/calculator.java
@@ -1,4 +1,7 @@
 class Calculator{
+	
+	// A class can contain multiple methods.
+	// A class will be contain only one main method
 	/*
 	operators
 	
@@ -18,15 +21,36 @@ class Calculator{
 		%   modulus
 		/   division
 		
+		public - access specifier ( optional)
+		static - keyword ( optional)
+		int 	-	return type	(Mandatory);
+		add		-	Method Name (Mandatory);
+		(int i, int j)	-	Parameters	(Optional)
+		
+		
+		Method implementation
+		
+		Method will be executed only when you call the method
+		
+
 	*/
+	
 	public static void main(String[] args){
 		int i = 100;
 		int j = 50;
 		int k = 100;
 		
-		int result1 = i + j;
+		
+		// Method call
+		int result1 = add(i , j);
 		System.out.println("additon result :: ["+ i + ","+j+"] = "+result1);
 		
+		int result5 = add(i , k);
+		System.out.println("additon result :: ["+ i + ","+k+"] = "+result5);
+		
+		int result6 = add( j, k);
+		System.out.println("additon result :: ["+ j + ","+k+"] = "+result);
+
 		int result2 = i - j;
 		System.out.println("Substraction result :: ["+ i + ","+j+"] = "+result2);
 		
@@ -55,4 +79,9 @@ class Calculator{
 		boolean eqFResult = i == j;
 		
 	}
+	
+	public static int add(int i, int j){
+		return i + j;
+	}
+	
 }
