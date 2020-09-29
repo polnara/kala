4db448e2daa9b0f456467101e1248bf027d01091
skala0123 <kalabee@yahoo.com>
2020-09-29 09:54:40 -0400

    i have committed pending hw assignments

--- programs/kalatest.java
+++ programs/kalatest.java
@@ -2,8 +2,11 @@ class kalatest {
     public static void main(String[] args) {
         int age = 30;
         int salary = 1000000;
+        int z = 0;
         
         Tax(age, salary);
+        z = Tax2(65,1400000);
+        System.out.println("tax is " +z);
     }
     public static void Tax(int age, int salary){
 		int tax = 0;
@@ -36,4 +39,35 @@ class kalatest {
         System.out.println("tax is " +(tax));
     }
     
+    public static int Tax2(int age, int salary){
+		int tax = 0;
+        if (salary >= 100000 && salary < 200000){
+            tax = salary * 5/100;
+            
+        }
+        else if(salary >=200000 && salary <500000){
+            tax = salary * 10/100;
+        }
+        else if (salary>=500000 && salary <1000000){
+            if (age>= 60){
+                tax = salary * 5/100;
+            }
+            else {
+                tax = salary * 15/100;
+            }
+          
+        }
+        else {
+            if (age>= 60){
+                tax = salary * 10/100;
+            }
+            
+        
+            else {
+                tax = salary * 20/100;
+                            }
+        }
+        return tax;
+        //System.out.println("tax is " +(tax));
+    }
 }
--- programs/whileloop.java
+++ programs/whileloop.java
@@ -5,6 +5,22 @@
              System.out.println(a);
              a--;
          }
+         add(4,7);
+         add(7,9);
+         add(3,7);
+         add(6,8);
+         add(2,8);
      }
-    
-}
+    public static void add(int a, int b){
+        //a = 3 b=5 3+4+5  return 12
+        int num = 0;
+        while(a<=b){
+            num = num + a;
+            a++;
+        }
+        System.out.println(num);
+       // return num;
+   
+   }
+    }
+
