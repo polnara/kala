4e2bbc31edeae9120281b524165dffeb7ae25514
Narasaiah <narsimcagips@gmail.com>
2020-10-05 20:18:19 +0530

    instance methods and variables

---
+++ programs/department.java
@@ -0,0 +1,25 @@
+class Department{
+	// Instance variables. We cannot access in any static methods
+	// we can create multiple objects for a class
+	String deptName = "IT";
+	int budget = 500000;
+	
+	// Instance method. It doesn't have static. Instance methods called through object
+	public static int monthlySpent(){
+		// non-static variable budget cannot be referenced from a static context
+		int monthlyBudget = budget / 12;
+		return monthlyBudget;
+	}
+	
+	public int weeklySpent(){
+		int weekSpent = monthlySpent() / 4;
+		return weekSpent;
+	}
+	public static void main(String[] args){
+		
+		Department dpt = new Department();
+		System.out.println("Monthly Spent :: " + dpt.monthlySpent());
+		System.out.println("Weekly spent :: " + dpt.weeklySpent());
+	}
+	
+}
